743301baa9a050a184ebc6935b13230c56b5107f
Famidore <lokoroko456@gmail.com>
2024-01-31 01:30:27 +0100

    basic setup

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from city import City
+from road import Road
+from map import Catan_Map
+
+c = City()
+r = Road()
+m = Catan_Map()
---
+++ src/map.py
@@ -0,0 +1,17 @@
+class Catan_Map:
+    """
+    A map class.
+
+    ```
+    Attributes
+    ----------
+    num_of_hexes : int = 19
+        number of hexes on the map
+
+    Methods
+    ------
+    ???
+    """
+
+    def __init__(self, num_of_hexes: int = 19):
+        self.num_of_hexes = num_of_hexes

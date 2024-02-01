9ae7b433f948986704a98ead1c21059fdbefeac2
Famidore <lokoroko456@gmail.com>
2024-02-01 21:34:59 +0100

    added hex number generator

---
+++ src/algorithms/hex_number_generator.py
@@ -0,0 +1,24 @@
+import random
+import matplotlib.pyplot as plt
+
+
+def generate_numbers(num_of_hexes: int = 19):
+    """
+    Generates a list of hex numbers given number of hexes
+    Based on gaussian distribution
+    """
+    mean = num_of_hexes / 2
+    sigma = num_of_hexes / 8
+    nums = [n for n in range(2, 13)]
+
+    while len(nums) < num_of_hexes:
+        n = int(random.gauss(mean, sigma))
+        if n in nums and n != 7:
+            nums.append(n)
+
+    return nums
+
+
+if __name__ == "__main__":
+    plt.hist(generate_numbers(), bins=19)
+    plt.show()

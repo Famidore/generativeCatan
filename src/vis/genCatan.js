199b557ece0a326b41804a18f3b6e4d65cbedef3
Famidore <lokoroko456@gmail.com>
2024-02-12 00:22:04 +0100

    generate hexes and simplify cities

--- src/vis/genCatan.js
+++ src/vis/genCatan.js
@@ -1,37 +1,43 @@
-let angle = 100
+let tiles = [];
+const tilesLength = 15;
+const tileSideLen = 150;
 
-function preload(){
-  villageMod = loadModel('objects/settlement.stl');
-  cityMod = loadModel('objects/city.stl');
+function preload() {
+  villageMod = loadModel('objects/simpleVillage.obj');
+  cityMod = loadModel('objects/simpleCity.obj');
+  hexMod = loadModel('objects/simpleHex.obj');
+  fontModel = loadFont('assets/genFont.otf')
 }
 
 function setup() {
-  createCanvas(800, 800, WEBGL);
-  village = new City(villageMod, cityMod);
-  city = new City(villageMod, cityMod)
-  normalMaterial();
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  textFont(fontModel);
+
+  village = new City(villageMod, cityMod, scaleValue = 0.1);
+  city = new City(villageMod, cityMod, scaleValue = 0.1);
+
+  for (let i = -tilesLength; i < tilesLength; i++) {
+    for (let j = -tilesLength; j < tilesLength; j++) {
+      tiles.push(new Hex(i * tileSideLen, 5, j * (tileSideLen - 10), hexMod, 0.2))
+    }
+  }
 }
 
 function draw() {
-  background(51);
-
   orbitControl();
 
-  push();
-  scale(6);
-  translate(0, 0);
-  rotateX(radians(45));
-  rotateZ(radians(270));
+  background(51);
+  rotateX(radians(340));
+
   city.show('city')
-  pop();
-
-  push();
-  scale(6);
-  translate(50, 0);
-  rotateX(radians(45));
-  rotateZ(radians(180));
   city.show('village')
-  pop();
-
-  angle++;
+  for (tile of tiles) {
+    tile.show();
+  }
 }
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+} 

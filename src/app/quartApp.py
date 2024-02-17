f875812ab6f9469c38c86d3453cfbceb8b03139f
Famidore <lokoroko456@gmail.com>
2024-02-17 15:15:41 +0100

    quart setup

---
+++ src/app/quartApp.py
@@ -0,0 +1,21 @@
+from quart import Quart, render_template, websocket
+import os
+import subprocess
+import asyncio
+
+app = Quart(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+async def index():
+    return await render_template("index.html")
+
+
+@app.route("/catan", methods=["GET", "POST"])
+async def catan():
+    # return await render_template("../../vis/index.html")
+    return await render_template("catan.html")
+
+
+if __name__ == "__main__":
+    asyncio.run(app.run_task(debug=True, host="0.0.0.0", port=5000))

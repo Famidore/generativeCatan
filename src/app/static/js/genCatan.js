eeb687ebd43ed520de05d5da7f743cba5dd044f1
Famidore <lokoroko456@gmail.com>
2024-02-17 20:30:29 +0100

    standard box test

--- src/app/static/js/genCatan.js
+++ src/app/static/js/genCatan.js
@@ -2,39 +2,40 @@ let tiles = [];
 const tilesLength = 15;
 const tileSideLen = 150;
 
-function preload() {
-  villageMod = loadModel('objects/simpleVillage.obj');
-  cityMod = loadModel('objects/simpleCity.obj');
-  hexMod = loadModel('objects/simpleHex.obj');
-  fontModel = loadFont('assets/genFont.otf')
-}
+// function preload() {
+//   villageMod = loadModel('objects/simpleVillage.obj');
+//   cityMod = loadModel('objects/simpleCity.obj');
+//   hexMod = loadModel('objects/simpleHex.obj');
+//   fontModel = loadFont('assets/genFont.otf')
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  textFont(fontModel);
+  // textFont(fontModel);
 
-  village = new City(villageMod, cityMod, scaleValue = 0.1);
-  city = new City(villageMod, cityMod, scaleValue = 0.1);
+  // village = new City(villageMod, cityMod, scaleValue = 0.1);
+  // city = new City(villageMod, cityMod, scaleValue = 0.1);
 
-  for (let i = -tilesLength; i < tilesLength; i++) {
-    for (let j = -tilesLength; j < tilesLength; j++) {
-      tiles.push(new Hex(i * tileSideLen, 5, j * (tileSideLen - 10), hexMod, 0.2))
-    }
-  }
+  // for (let i = -tilesLength; i < tilesLength; i++) {
+  //   for (let j = -tilesLength; j < tilesLength; j++) {
+  //     tiles.push(new Hex(i * tileSideLen, 5, j * (tileSideLen - 10), hexMod, 0.2))
+  //   }
+  // }
 }
 
 function draw() {
   orbitControl();
 
-  background(51);
+  background(69);
   rotateX(radians(340));
 
-  city.show('city')
-  city.show('village')
-  for (tile of tiles) {
-    tile.show();
-  }
+  // city.show('city')
+  // city.show('village')
+  // for (tile of tiles) {
+  //   tile.show();
+  // }
+  box(100,100);
 }
 
 
